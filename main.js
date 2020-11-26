56660c753b19d49fa98c438a3aa7ec8c96193834
qmohitsingh <qmohitsingh@gmail.com>
2020-11-27 02:35:00 +0530

    removed console function :refactor:

--- main.js
+++ main.js
@@ -2,13 +2,10 @@ const fs = require('fs');
 
 if (require.main === module) {
     let filename = process.argv[2];
-    console.log('filename: ', filename)
+
     let inputData = fs.readFileSync(filename);
 
-    console.log(inputData.toString());
-    console.log(inputData.toString().split('\n'))
     inputData = inputData.toString().split('\n');
-
 
     //snakes input format
     let snakes = {};
@@ -18,7 +15,6 @@ if (require.main === module) {
         snakes[parseInt(inputData[i].split(' ')[0])] = parseInt(inputData[i].split(' ')[1]);
     }
 
-    //console.log(snakes)
     //ladders
     let ladders = {};
     let numOfLadders = i + parseInt(inputData[i++]);
@@ -26,14 +22,10 @@ if (require.main === module) {
         ladders[parseInt(inputData[i].split(' ')[0])] = parseInt(inputData[i].split(' ')[1]);
     }
 
-    //console.log(ladders)
-
     //users info
     let users = [];
     let numOfUsers = i + parseInt(inputData[i++]);
     for(; i<=numOfUsers; i++) {
         users.push(inputData[i]);
     }
-
-    //console.log(users)
 }
